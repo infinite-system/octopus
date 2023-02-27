b53453035a2fa8480184d6d1258b5095252f2afc
Evgeny Kalashnikov <ekalashnikov@gmail.com>
2023-02-26 21:54:58 -0500

    minor test improvements

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -21,6 +21,30 @@ class CategoryTest extends TestCase
         $this->assertEquals(25, $user->filling_user_profile->id);
     }
 
+    public function testEagerCategoryOne()
+    {
+        $user = User::where('id', 15)->with('billing_user_profile')->first();
+        $this->assertEquals(24, $user->billing_user_profile->id);
+    }
+
+    public function testEagerCategoryOneAnotherSet()
+    {
+        $user = User::where('id', 16)->with('filling_user_profile')->first();
+        $this->assertEquals(25, $user->filling_user_profile->id);
+    }
+
+    public function testLazyCategoryMany()
+    {
+        $user = User::where('id', 17)->first();
+        $this->assertEquals([26, 27], $user->billing_user_profiles->pluck('id')->all());
+    }
+
+    public function testLazyCategoryManyAnotherSet()
+    {
+        $user = User::where('id', 18)->first();
+        $this->assertEquals([28,29], $user->filling_user_profiles->pluck('id')->all());
+    }
+
     public function testEagerCategoryMany()
     {
         $user = User::where('id', 17)->with('billing_user_profiles')->first();
