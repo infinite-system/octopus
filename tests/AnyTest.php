2d87aee357e82c5d7df69e16571a561e47f7f0dd
Evgeny K <evgeny@triumphadvertisinginc.com>
2023-03-09 14:05:46 -0500

    any test

--- tests/AnyTest.php
+++ tests/AnyTest.php
@@ -5,6 +5,10 @@ use Tests\Models\User;
 
 class AnyTest extends TestCase
 {
-
+	public function testLazyLoading2LevelsOne()
+	{
+		$user = User::where('id', 8)->first();
+		$this->assertEquals(21, $user->contact->user_profile->id);
+	}
 }
 
