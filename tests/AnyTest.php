b0fa08168462eb338c987a5224a75d57af97d97d
Evgeny K <evgeny@triumphadvertisinginc.com>
2023-03-09 15:13:30 -0500

    any tests added

--- tests/AnyTest.php
+++ tests/AnyTest.php
@@ -1,14 +1,25 @@
 <?php
 namespace Tests;
 
-use Tests\Models\User;
+use Tests\Models\Order;
 
 class AnyTest extends TestCase
 {
-	public function testLazyLoading2LevelsOne()
+	public function testAnyActualTag()
 	{
-		$user = User::where('id', 8)->first();
-		$this->assertEquals(21, $user->contact->user_profile->id);
+		$order = Order::where('id', 31)->first();
+//		dump($order->order_category->toArray());
+		$this->assertEquals(12, $order->order_category->target_type);
+	}
+	public function testAnyConnection()
+	{
+		$order = Order::where('id', 31)->first();
+		$this->assertEquals(1, $order->order_category_any[0]->target_type);
+		$this->assertEquals(21, $order->order_category_any[0]->target_id);
+		$this->assertEquals(8, $order->order_category_any[1]->target_type);
+		$this->assertEquals(24, $order->order_category_any[1]->target_id);
+		$this->assertEquals(11, $order->order_category_any[2]->target_type);
+		$this->assertEquals(11, $order->order_category_any[2]->target_id);
 	}
 }
 
